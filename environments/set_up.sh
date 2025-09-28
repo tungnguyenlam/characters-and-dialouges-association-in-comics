90e380b1fbae55452d1195a52bbb75a62413a45c
tungnguyenlam <nguyenlamtung2005@gmail.com>
2025-09-28 17:11:53 +0700

    Create folder output

--- environments/set_up.sh
+++ environments/set_up.sh
@@ -3,21 +3,14 @@
 if [[ "$(pwd)" == *"/characters-and-dialouges-association-in-comics" ]] || [[ "$(pwd)" == *"/workspace" ]]; then
     echo "You are in the characters-and-dialouges-association-in-comics directory or /workspace"
 
-    if [ ! -d ./data/Manga109 ]; then
-        gdown --fuzzy "https://drive.google.com/file/d/1xfrYRbLFK1Nzi6Yc3bKEJZ-llPBJOG2w/view?usp=sharing" -O ./data/Manga109_zipped.zip 
-        unzip -o ./data/Manga109_zipped.zip -d ./data/Manga109
+    if [ ! -d ./data/Manga109_re_2023_12_07 ]; then
+        gdown --fuzzy "https://drive.google.com/file/d/1ZvGD7g_7l9RwxnVV2KEz61BdXkzTDyBT/view?usp=sharing" -O ./data/Manga109_zipped.zip 
+        unzip -o ./data/Manga109_zipped.zip -d ./data/
         rm ./data/Manga109_zipped.zip
     else
-        echo "./data/Manga109 already exists, skipping download."
+        echo "./data/Manga109_re_2023_12_07 already exists, skipping download."
     fi
 
-    if [ ! -d ./data/Manga109_Dialouge ]; then
-        gdown --fuzzy "https://drive.google.com/file/d/1ON4TPEKleFJX0RyIiMlg__W3YSkN9tNc/view?usp=sharing" -O ./data/Manga109_Dialouge_zipped.zip
-        unzip -o ./data/Manga109_Dialouge_zipped.zip -d ./data/Manga109_Dialouge
-        rm ./data/Manga109_Dialouge_zipped.zip
-    else
-        echo "./data/Manga109_Dialouge already exists, skipping download."
-    fi
 
 else
     echo "You are NOT in the characters-and-dialouges-association-in-comics directory or /workspace"
