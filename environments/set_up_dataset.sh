7b6a512d73785adc52baa192b92f561ab2ad0438
hngocngvu <hongngocnguyenvu.work@gmail.com>
2025-10-02 05:04:14 +0000

    Update changes to the set_up_dataset.sh

--- environments/set_up_dataset.sh
+++ environments/set_up_dataset.sh
@@ -14,3 +14,21 @@ unzip ./data/Manga109_released_2023_12_07.zip -d ./data
 rm ./data/Manga109_released_2023_12_07.zip
 rm -rf ./data/.cache
 rm -rf ./data/__MACOSX
+
+
+if [[ "$(pwd)" == *"/characters-and-dialouges-association-in-comics" ]] || [[ "$(pwd)" == *"/workspace" ]]; then
+    echo "You are in the characters-and-dialouges-association-in-comics directory or /workspace"
+
+    if [ ! -d ./data/Human_Annotate_300 ]; then
+        gdown --fuzzy "https://drive.google.com/file/d/1nekCTGInk57Oe2cZvOjO7RRkUpS2mevw/view?usp=sharing" -O ./data/Human_Annotate_300.zip 
+        unzip -o ./data/Human_Annotate_300.zip -d ./data/
+        rm ./data/Human_Annotate_300.zip
+    else
+        echo "./data/Human_Annotate_300 already exists, skipping download."
+    fi
+
+
+else
+    echo "You are NOT in the characters-and-dialouges-association-in-comics directory or /workspace"
+    echo "Change the directory to characters-and-dialouges-association-in-comics in local or /workspace in docker"
+fi
