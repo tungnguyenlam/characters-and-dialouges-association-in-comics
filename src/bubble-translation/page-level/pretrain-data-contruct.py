a9752570a3bc1cbfdb60afc3ef62c1f545af3413
tungnguyen <nguyenlamtung2005@gmail.com>
2026-01-16 17:22:48 +0700

    Rename full finetune to pretrain

--- src/bubble-translation/page-level/pretrain-data-contruct.py
+++ src/bubble-translation/page-level/pretrain-data-contruct.py
@@ -10,8 +10,8 @@ from dotenv import load_dotenv
 load_dotenv(os.path.join(cwd, "..", ".env"))
 HF_TOKEN = os.getenv("HF_TOKEN")
 
-SAVE_DIR = os.path.join(cwd, "data", "translation", "full-finetune")
-SAVE_FILE = os.path.join(SAVE_DIR, "full-finetune-data.jsonl")
+SAVE_DIR = os.path.join(cwd, "data", "translation", "pretrain")
+SAVE_FILE = os.path.join(SAVE_DIR, "pretrain-data.jsonl")
 os.makedirs(SAVE_DIR, exist_ok=True)
 
 # Create data loader for JESC 2019
